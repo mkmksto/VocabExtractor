2c85371acf20b3eab7740806128ffae1ea96dc2d
Michael Spencer Quinto <SpencerMAQ@users.noreply.github.com>
2021-07-16 16:56:11 +0800

    barebones python class and config

--- plugin/extractor.py
+++ plugin/extractor.py
@@ -2,8 +2,28 @@
 # Copyright: Tanaka Aiko (https://github.com/aiko-tanaka)
 # License: GNU AGPL, version 3 or later; https://www.gnu.org/licenses/agpl-3.0.en.html
 
+# parts of the code are copied from renato's work (https://github.com/rgamici)
+
 from io import StringIO
 from html.parser import HTMLParser
+
+if False:
+    from PyQt5.QtWidgets import *
+    from PyQt5.QtCore import *
+    from PyQt5.QtGui import *
+    from aqt import mw
+
+# Variables controlled by the user (can be edited on Addons > Config)
+config = mw.addonManager.getConfig(__name__)
+expressionField = config['expressionField']
+definitionField = config['definitionField']
+keybinding = config['keybinding'] #nothing by default
+
+# Labels
+# text shown while processing cards
+label_progress_update = 'Generating Japanese definitions...'
+# text shown on menu to run the functions
+label_menu = 'Extract Vocab wrapped in <b></b> from Sentence field'
 
 # https://stackoverflow.com/questions/2081640/what-exactly-do-u-and-r-string-flags-do-and-what-are-raw-string-literals
 class MLStripper(HTMLParser):
@@ -23,6 +43,44 @@ def strip_tags(html):
     s.feed(html)
     return s.get_data()
 
-# text = r"<ruby><rb>僕</rb><rt>ぼく</rt></ruby>は<ruby><rb>最初</rb><rt>さいしょ</rt></ruby>から<ruby><rb>危険</rb><rt>きけん</rt></ruby>を<b><ruby><rb>犯</rb><rt>おか</rt></ruby>している</b>じゃないか、"
-# print(strip_tags(text))
+# if string/vocab is empty, do nothing
+
+class Regen():
+    def __init__(self, ed, fids):
+        self.ed = ed
+        self.fids = fids
+        self.completed = 0
+        self.config = mw.addonManager.getConfig(__name__)
+        if len(self.fids) == 1:
+            # Single card selected, need to deselect it before updating
+            self.row = self.ed.currentRow()
+            self.ed.form.tableView.selectionModel().clear()
+        mw.progress.start(max=len(self.fids), immediate=True)
+        mw.progress.update(
+            label=label_progress_update,
+            value=0)
+
+
+def setupMenu(ed):
+    """Add entry in Edit menu"""
+    a = QAction(label_menu, ed)
+    a.triggered.connect(lambda _, e=ed: onRegenGlosses(e))
+    ed.form.menuEdit.addAction(a)
+    a.setShortcut(QKeySequence(keybinding))
+
+
+def addToContextMenu(view, menu):
+    """Add entry to context menu (right click)"""
+    menu.addSeparator()
+    a = menu.addAction(label_menu)
+    a.triggered.connect(lambda _, e=view: onRegenGlosses(e))
+    a.setShortcut(QKeySequence(keybinding))
+
+
+# def onRegenGlosses(ed):
+#     """ """
+#     regen = Regen(ed, ed.selectedNotes())
+#     regen.prepare()
+#     regen.wait_threads()
+#     mw.requireReset()
 
