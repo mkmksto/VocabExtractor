291d7748d411e3613f6efb83b6efa821fdd09f92
Michael Spencer Quinto <SpencerMAQ@users.noreply.github.com>
2021-07-30 18:08:29 +0800

    implemented a jisho deconjugator (not yet in Anki class)

--- plugin/extractor.py
+++ plugin/extractor.py
@@ -11,14 +11,19 @@
 
 # TODO: you can use ichi.moe's vocab info to deconjugate stuff, but idk, i'm too lazy, haven't felt the need for it yet
 
-from io import StringIO
 from html.parser import HTMLParser
+from bs4 import BeautifulSoup
+from io import StringIO
+import urllib.request
+import urllib.parse
 
 import traceback
+import json
+import time
 import os
 import re
 
-test_in_anki = True
+test_in_anki = False
 
 dir_path = os.path.dirname(os.path.realpath(__file__)).split("\\")[-1]
 # print(dir_path)
@@ -118,123 +123,161 @@ def remove_ruby(sentence):
     except Exception as e:
         showInfo('failed to remove furigana - {}'.format(str(e)))
 
-class Regen():
-    """Used to organize the work flow to update the selected cards
-       Attributes
-       ----------
-       ed :
-           Anki Card browser object
-       fids :
-           List of selected cards
-       completed : int
-           Track how many cards were already processed
-       """
-    def __init__(self, ed, fids):
-        self.ed         = ed
-        # ed.selectedNotes
-        self.fids       = fids
-        self.completed  = 0
-        # self.config     = mw.addonManager.getConfig(__name__)
-        if len(self.fids) == 1:
-            # Single card selected, need to deselect it before updating
-            self.row = self.ed.currentRow()
-            self.ed.form.tableView.selectionModel().clear()
-        mw.progress.start(max=len(self.fids), immediate=True)
-        mw.progress.update(
-            label=label_progress_update,
-            value=0)
 
-    def _update_progress(self):
-        self.completed += 1
-        mw.progress.update(
-            label=label_progress_update,
-            value=self.completed)
-        if self.completed >= len(self.fids):
-            mw.progress.finish()
-            return
+def jisho_deconjugate(vocab):
+    """
+    https://github.com/lsrdg/jisho-karini/blob/master/jisho-karini.py
+    https://jisho.org/forum/54fefc1f6e73340b1f160000-is-there-any-kind-of-search-api
+    """
 
-    def generate(self):
-        fs = [mw.col.getNote(id=fid) for fid in self.fids]
+    url = 'https://jisho.org/api/v1/search/words?keyword={}'.format(urllib.parse.quote(vocab.encode('utf-8')))
 
-        for f in fs:
-            # empty sentence field (probably very rare)
-            if not f[expression_field]:
-                continue
-
-            sent = str(f[expression_field])
-            if '[' in sent and expression_field == 'Reading':
-                sent = remove_furigana(sent)
-            elif '<rt>' in sent and expression_field == 'Reading':
-                sent = remove_ruby(sent)
-
+    num_retries = 10
+    try:
+        response = urllib.request.urlopen(url)
+    except Exception as e:
+        for i in range(num_retries):
             try:
-                # vocab field already contains something
-                if force_update == 'no' and f[vocab_field]:
-                    # do nothing, count it as progress
-                    self._update_progress()
-                    mw.progress.finish()
-                    continue
-
-                elif not f[vocab_field] and r"<b>" in sent:
-                    # vocab_field is empty, fill it
-                    f[vocab_field] = get_vocab(sent)
-                    self._update_progress()
-                    mw.progress.finish()
-
-                elif force_update == 'yes' and f[vocab_field] and r"<b>" in sent:
-                    f[vocab_field] += get_vocab(sent)
-                    self._update_progress()
-                    mw.progress.finish()
-
-                else:
-                    pass
-
+                response = urllib.request.urlopen(url)
             except Exception as e:
-                showInfo('error from generate() function, - {}'.format(str(e)))
+                time.sleep(0.02)
 
-            try:
-                f.flush()
-            except Exception as e:
-                pass
+    response = BeautifulSoup(response, features='html.parser')
 
-            # just a fail-safe
+    if response:
+        response_json = json.loads(str(response))
+        response_data = response_json['data'][0]
+        # print(response_data)
+        deconjugated = response_data['slug']
+    else:
+        return
+
+    return deconjugated
+
+
+if test_in_anki:
+    class Regen():
+        """Used to organize the work flow to update the selected cards
+           Attributes
+           ----------
+           ed :
+               Anki Card browser object
+           fids :
+               List of selected cards
+           completed : int
+               Track how many cards were already processed
+           """
+        def __init__(self, ed, fids):
+            self.ed         = ed
+            # ed.selectedNotes
+            self.fids       = fids
+            self.completed  = 0
+            # self.config     = mw.addonManager.getConfig(__name__)
+            if len(self.fids) == 1:
+                # Single card selected, need to deselect it before updating
+                self.row = self.ed.currentRow()
+                self.ed.form.tableView.selectionModel().clear()
+            mw.progress.start(max=len(self.fids), immediate=True)
+            mw.progress.update(
+                label=label_progress_update,
+                value=0)
+
+        def _update_progress(self):
+            self.completed += 1
+            mw.progress.update(
+                label=label_progress_update,
+                value=self.completed)
             if self.completed >= len(self.fids):
                 mw.progress.finish()
-                showInfo('Extraction done for {} out of {} notes done'.format(
-                                                                        self.completed,
-                                                                        len(self.fids)
-                                                                        ))
-
                 return
 
-def setup_menu(ed):
-    """
-    Add entry in Edit menu
-    """
-    a = QAction(label_menu, ed)
-    a.triggered.connect(lambda _, e=ed: on_regen_vocab(e))
-    ed.form.menuEdit.addAction(a)
-    a.setShortcut(QKeySequence(keybinding))
+        def generate(self):
+            fs = [mw.col.getNote(id=fid) for fid in self.fids]
+
+            for f in fs:
+                # empty sentence field (probably very rare)
+                if not f[expression_field]:
+                    continue
+
+                sent = str(f[expression_field])
+                if '[' in sent and expression_field == 'Reading':
+                    sent = remove_furigana(sent)
+                elif '<rt>' in sent and expression_field == 'Reading':
+                    sent = remove_ruby(sent)
+
+                try:
+                    # vocab field already contains something
+                    if force_update == 'no' and f[vocab_field]:
+                        # do nothing, count it as progress
+                        self._update_progress()
+                        mw.progress.finish()
+                        continue
+
+                    elif not f[vocab_field] and r"<b>" in sent:
+                        # vocab_field is empty, fill it
+                        f[vocab_field] = get_vocab(sent)
+                        self._update_progress()
+                        mw.progress.finish()
+
+                    elif force_update == 'yes' and f[vocab_field] and r"<b>" in sent:
+                        f[vocab_field] += get_vocab(sent)
+                        self._update_progress()
+                        mw.progress.finish()
+
+                    else:
+                        pass
+
+                except Exception as e:
+                    showInfo('error from generate() function, - {}'.format(str(e)))
+
+                try:
+                    f.flush()
+                except Exception as e:
+                    pass
+
+                # just a fail-safe
+                if self.completed >= len(self.fids):
+                    mw.progress.finish()
+                    showInfo('Extraction done for {} out of {} notes done'.format(
+                                                                            self.completed,
+                                                                            len(self.fids)
+                                                                            ))
+
+                    return
+
+    def setup_menu(ed):
+        """
+        Add entry in Edit menu
+        """
+        a = QAction(label_menu, ed)
+        a.triggered.connect(lambda _, e=ed: on_regen_vocab(e))
+        ed.form.menuEdit.addAction(a)
+        a.setShortcut(QKeySequence(keybinding))
 
 
-def add_to_context_menu(view, menu):
-    """
-    Add entry to context menu (right click)
-    """
-    menu.addSeparator()
-    a = menu.addAction(label_menu)
-    a.triggered.connect(lambda _, e=view: on_regen_vocab(e))
-    a.setShortcut(QKeySequence(keybinding))
+    def add_to_context_menu(view, menu):
+        """
+        Add entry to context menu (right click)
+        """
+        menu.addSeparator()
+        a = menu.addAction(label_menu)
+        a.triggered.connect(lambda _, e=view: on_regen_vocab(e))
+        a.setShortcut(QKeySequence(keybinding))
 
 
-def on_regen_vocab(ed):
-    """
-    main function
-    """
-    regen = Regen(ed, ed.selectedNotes())
-    regen.generate()
-    mw.reset()
-    mw.requireReset()
+    def on_regen_vocab(ed):
+        """
+        main function
+        """
+        regen = Regen(ed, ed.selectedNotes())
+        regen.generate()
+        mw.reset()
+        mw.requireReset()
 
-addHook('browser.setupMenus', setup_menu)
-addHook('browser.onContextMenu', add_to_context_menu)
+    addHook('browser.setupMenus', setup_menu)
+    addHook('browser.onContextMenu', add_to_context_menu)
+
+# testing inside pycharm
+sample_word = '食べちゃった'
+if __name__ == '__main__':
+    print(jisho_deconjugate(sample_word))
